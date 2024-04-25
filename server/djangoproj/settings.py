ee32452d08456a2e64cadc6579b7ed9af0438085
Erik Cheung <>
2024-04-25 00:52:12 -0400

    Add NLP and implement restapis

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost',
+    'https://erikcfch-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
